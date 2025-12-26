e6b7d19d788c9919e0c2c9caabc313ebaa86f7df
isaac.scerri <isaac.scerri@gmail.com>
2025-12-26 20:50:48 +0000

    Got the DB on local environment

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,13 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
+    pkgs.python311
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.sqlalchemy
+    pkgs.python311Packages.databases
+    pkgs.python311Packages.psycopg2
+    pkgs.postgresql
     pkgs.nodejs_20
   ];
   idx.extensions = [
